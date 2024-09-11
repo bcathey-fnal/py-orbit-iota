2ae03bbd61bb1501020dc5bb4f3983b7bd8478a2
Nilanjan Banerjee <nilanjan@fnal.gov>
2024-09-10 20:12:14 -0500

    Corrected some issues with module imports.

---
+++ py/orbit/space_charge/analytical/__init__.py
@@ -0,0 +1,6 @@
+from orbit.space_charge.analytical.scAccNodes import SCanalyticalAccNode
+from orbit.space_charge.analytical.scLatticeModifications import setSCanalyticalAccNodes
+
+__all__ = []
+__all__.append("SCanalyticalAccNode")
+__all__.append("setSCanalyticalAccNodes")
